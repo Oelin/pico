eeed7afa050e58d8c4a5a3f66cf235fe0d4a1586
Cloud User <ec2-user@ip-172-31-27-14.us-east-2.compute.internal>
2020-08-12 12:00:10 +0000

    first commit

---
+++ pico.js
@@ -0,0 +1,21 @@
+const http = require('http')
+const db = {}
+
+
+const pico = ({url}, res) => {
+
+  if (url.startsWith('/@')) {
+    len = Object.keys(db).length
+    db[len] = url.slice(2)
+    res.end(len)
+  }
+
+  else {
+    res.writeHead(302, {'location': db[url.slice(1)] || ''})
+    res.end()
+  }
+}
+
+
+http.createServer(pico)
+.listen(80)
---
+++ readme.md
@@ -0,0 +1,3 @@
+## pico
+
+A simple URL shortener.
